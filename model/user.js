e4c4662232e43ee3860d540f4443d9659459c6a6
elsa-kim <ksh96611@naver.com>
2022-12-22 22:16:26 +0900

    12/22
    비밀번호 암호화(bcrypt 사용)

--- model/user.js
+++ model/user.js
@@ -11,6 +11,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  name: {
+    type: String,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
